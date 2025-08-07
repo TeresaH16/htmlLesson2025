47d503fd7eac7820da89a9728b34cf3b917f79a5
TeresaH16 <heredia.t@icloud.com>
2025-08-06 23:03:18 -0400

    Add files via upload

---
+++ retro-polaroid/js/footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const footer = `
+    <footer>
+      <p>&copy; 2025 Polaroid Classic. All rights reserved.</p>
+    </footer>
+  `;
+  document.body.insertAdjacentHTML("beforeend", footer);
+});
